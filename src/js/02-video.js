d1e236ee01ea82753d89361ebaaf768dfb4fb8a7
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-07-03 14:30:09 +0200

    3-3 HW 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+// import Player from '@vimeo/player';
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import storageApi from './storage';
 
+// Створіть гравця
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const KEY_STORAGE = 'videoplayer-current-time';
+
+player.setCurrentTime(storageApi.load(KEY_STORAGE)).then(function(seconds) {
+    seconds = storageApi.load(KEY_STORAGE);
+});
+
+player.on('timeupdate', throttle(function(event) {  
+    storageApi.save(KEY_STORAGE, event.seconds);
+}, 1000));
